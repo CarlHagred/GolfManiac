6af92297c27ff8d85387c3ee4805264830f1b7d3
JoelSven <62708808+JoelSven@users.noreply.github.com>
2021-04-20 15:37:11 +0200

    Drag and shoot

---
+++ Assets/Joel&Gustav/Shoot.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public float power = 10f;
+    public Rigidbody2D rb;
+
+    public Vector2 minPower;
+    public Vector2 maxPower;
+
+    Trajector traj;
+
+    Camera cam;
+    Vector2 force;
+    Vector3 startPoint;
+    Vector3 endPoint;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        traj = GetComponent<Trajector>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            startPoint.z = 15;
+            Debug.Log(startPoint);
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 currentPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            currentPoint.z = 15;
+            traj.RenderLine(startPoint, currentPoint);
+
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            endPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            endPoint.z = 15;
+
+            force = new Vector2(Mathf.Clamp(startPoint.x - endPoint.x, minPower.x, maxPower.x), Mathf.Clamp(startPoint.y - endPoint.y, minPower.y, maxPower.y));
+            rb.AddForce(force * power, ForceMode2D.Impulse);
+            traj.EndLine();
+        }
+    }
+
+
+}
